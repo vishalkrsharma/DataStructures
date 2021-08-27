fbcca4c24227494024d9df13341b79c9629de74e
vishalkrsharma <vishalkrsharma1234@gmail.com>
2021-08-27 15:57:04 +0530

    Queue

---
+++ Queue.java
@@ -0,0 +1,42 @@
+public class Queue <T> implements Iterable <T> {
+
+  private java.util.LinkedList <T> list = new java.util.LinkedList <T> ();
+
+  public Queue() {}
+
+  public Queue(T firstElem) {
+    offer(firstElem);
+  } 
+
+  public int size() {
+    return list.size();
+  }
+
+  public boolean isEmpty() {
+    return size()==0;
+  }
+
+  public T peek() {
+    if(isEmpty()) {
+      throw new RuntimeException("Queue Empty");
+    }
+    return list.peekFirst();
+  }
+
+  public T poll() {
+    if(isEmpty()) {
+      throw new RuntimeException("Queue Empty");
+    }
+    return list.removeFirst();
+  }
+
+  public void offer(T elem) {
+    list.addLast(elem);
+  }
+
+  @Override
+  public java.util.Iterator <T> iterator() {
+    return list.iterator();
+  }
+
+}
